0387a0d8e2d6556f53a269208a641ecc73c1c339
Nicole Williams <nicole.boesenberg@gmail.com>
2023-06-10 12:36:14 -0700

    completed part3

--- melons.py
+++ melons.py
@@ -8,7 +8,6 @@ class AbstractMelonOrder:
     def __init__(self,species,qty):
         self.species = species
         self.qty = qty
-        # self.tax = tax 
     
     def mark_shipped(self):
         """Record the fact than an order has been shipped."""
@@ -29,6 +28,7 @@ class AbstractMelonOrder:
     def __repr__(self):
         return f'Species: {self.species}, Qty: {self.qty}, Tax: {self.tax}'
 
+
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
     tax = 0.08
@@ -39,22 +39,6 @@ class DomesticMelonOrder(AbstractMelonOrder):
 
         self.species = species
         self.qty = qty
-        # self.shipped = False
-        # self.order_type = "domestic"
-        # self.tax = 0.08
-
-    # def get_total(self):
-    #     """Calculate price, including tax."""
-
-    #     base_price = 5
-    #     total = (1 + self.tax) * self.qty * self.base_price
-
-    #     return total
-
-    # def mark_shipped(self):
-    #     """Record the fact than an order has been shipped."""
-
-    #     self.shipped = True
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
@@ -68,24 +52,16 @@ class InternationalMelonOrder(AbstractMelonOrder):
         self.species = species
         self.qty = qty
         self.country_code = country_code
-        # self.shipped = False
-        # self.order_type = "international"
-        # self.tax = 0.17
-
-    # def get_total(self):
-    #     """Calculate price, including tax."""
-
-    #     base_price = 5
-    #     total = (1 + self.tax) * self.qty * base_price
-
-    #     return total
-
-    # def mark_shipped(self):
-    #     """Record the fact than an order has been shipped."""
-
-    #     self.shipped = True
 
     def get_country_code(self):
         """Return the country code."""
 
         return self.country_code
+
+class GovernmentMelonOrder(DomesticMelonOrder, AbstractMelonOrder):
+    tax = 0
+    passed_inspection = False
+
+    def mark_inspection(self, passed):
+        if passed == True:
+            self.passed_inspection = True
